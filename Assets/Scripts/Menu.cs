f0ba240f52cd18f14fc797ce85f6350157686f21
robotdognz <robotdognz@gmail.com>
2022-11-27 12:51:44 +1300

    Added start of menu system

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] Button[] menuButtons;
+    [SerializeField] GameObject titleScreen;
+    [SerializeField] GameObject instructions;
+    [SerializeField] GameObject win;
+    [SerializeField] GameObject fail;
+
+    public void ActivateMenu()
+    {
+        if (menuButtons != null && menuButtons.Length > 0)
+        {
+            foreach (Button b in menuButtons)
+            {
+                b.gameObject.SetActive(true);
+            }
+        }
+        titleScreen.SetActive(true);
+        instructions.SetActive(false);
+        win.SetActive(false);
+        fail.SetActive(false);
+    }
+
+    public void DeactivateMenu()
+    {
+        if (menuButtons != null && menuButtons.Length > 0)
+        {
+            foreach (Button b in menuButtons)
+            {
+                b.gameObject.SetActive(false);
+            }
+        }
+        titleScreen.SetActive(false);
+        instructions.SetActive(false);
+        win.SetActive(false);
+        fail.SetActive(false);
+    }
+
+    public void StartNewGame()
+    {
+        Debug.Log("Started new game");
+        // start the game
+    }
+
+    public void ShowInstructions()
+    {
+        Debug.Log("Display instructions");
+        titleScreen.SetActive(false);
+        instructions.SetActive(true);
+        win.SetActive(false);
+        fail.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit game");
+        // quit the game
+    }
+}
